eb37f72bf189a3559d5e818378767fef6c2aa9c3
gonzalespaulb <76889345+gonzalespaulb@users.noreply.github.com>
2023-01-11 19:00:37 -0700

    yap tab functioality good

--- client/src/Controls/Controls.jsx
+++ client/src/Controls/Controls.jsx
@@ -4,12 +4,30 @@ import {
   Login,
   LoginCreate,
   MainContainer,
+  Nav,
+  Logout,
   ValueInput,
+  YapAccount,
+  ProfileName,
+  YapControls,
+  YapTabContainer,
+  YapTab,
+  Divider,
+  NewYapContainer,
+  YapItBtn,
+  MyYapsContainer,
+  YapContainer,
+  YapDate,
+  Yap,
 } from "./styles";
+import { useState } from "react";
 
 const Controls = () => {
-  return (
-    <MainContainer>
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [seeMyYaps, setSeeMyYaps] = useState(false);
+
+  const renderLogin = () => {
+    return (
       <Login>
         <InputContainer>
           <InputLabel>Username</InputLabel>
@@ -21,9 +39,81 @@ const Controls = () => {
           <ValueInput></ValueInput>
         </InputContainer>
 
-        <LoginCreate>Login</LoginCreate>
+        <LoginCreate onClick={() => setLoggedIn(true)}>Login</LoginCreate>
         <LoginCreate>Create Account</LoginCreate>
       </Login>
+    );
+  };
+
+  const newYap = () => {
+    return (
+      <NewYapContainer>
+        <InputContainer>
+          <InputLabel>What's your yap?</InputLabel>
+          <ValueInput></ValueInput>
+        </InputContainer>
+        <YapItBtn>Yap It!</YapItBtn>
+      </NewYapContainer>
+    );
+  };
+
+  const myYaps = () => {
+    return (
+      <MyYapsContainer>
+      <YapContainer>
+        <YapDate>July 22, 2019</YapDate>
+        <Yap>
+          First yap of the day, this is my practice for database stuff.
+        </Yap>
+      </YapContainer>
+
+      <YapContainer>
+        <YapDate>July 22, 2019</YapDate>
+        <Yap>
+          First yap of the day, this is my practice for database stuff.
+        </Yap>
+      </YapContainer>
+
+      <YapContainer>
+        <YapDate>July 22, 2019</YapDate>
+        <Yap>
+          First yap of the day, this is my practice for database stuff.
+        </Yap>
+      </YapContainer>
+
+      <YapContainer>
+        <YapDate>July 22, 2019</YapDate>
+        <Yap>
+          First yap of the day, this is my practice for database stuff.
+        </Yap>
+      </YapContainer>
+    </MyYapsContainer>
+    )
+  };
+
+  const renderYapAccount = () => {
+    return (
+      <YapAccount>
+        <Nav>
+          <ProfileName>Paulie</ProfileName>
+          <Logout onClick={() => setLoggedIn(false)}>Logout</Logout>
+        </Nav>
+        <YapControls>
+          <YapTabContainer>
+            <YapTab onClick={() => setSeeMyYaps(false)} seeMyYaps={seeMyYaps} tabType={false}>New Yap</YapTab>
+            <YapTab onClick={() => setSeeMyYaps(true)} seeMyYaps={seeMyYaps}  tabType={true}>My Yaps</YapTab>
+          </YapTabContainer>
+        </YapControls>
+        <Divider />
+
+        {seeMyYaps ? myYaps() : newYap()}
+      </YapAccount>
+    );
+  };
+
+  return (
+    <MainContainer>
+      {loggedIn ? renderYapAccount() : renderLogin()}
     </MainContainer>
   );
 };
--- client/src/Controls/styles.js
+++ client/src/Controls/styles.js
@@ -1,51 +1,167 @@
 import styled from "styled-components";
 
 export const MainContainer = styled.div`
-    height: 100vh;
-    position: sticky;
-    top: 0;
-    padding: 100px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+  height: 100vh;
+  position: sticky;
+  top: 0;
+  padding: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
+// NOTE ---------------------------------------------- LOGIN
+
 export const Login = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 `;
 
 export const InputContainer = styled.div`
-    display: flex;
-    width: 100%;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
 
-    :not(:last-child) {
-        margin-bottom: 24px;
-    }
+  :not(:last-child) {
+    margin-bottom: 24px;
+  }
 `;
 
 export const InputLabel = styled.span`
-    font-size: 24px;
-    margin-bottom: 16px;
+  font-size: 24px;
+  margin-bottom: 16px;
 `;
 
 export const ValueInput = styled.input`
-    width: 100%;
-    height: 48px;
+  width: 100%;
+  height: 48px;
 `;
 
 export const LoginCreate = styled.span`
-    cursor: pointer;
-    font-size: 24px;
+  cursor: pointer;
+  font-size: 24px;
 
-    :hover {
-        text-decoration: underline;
-    }
+  :hover {
+    text-decoration: underline;
+  }
 `;
+
+// NOTE ---------------------------------------------- YAP ACCOUNT
+
+export const YapAccount = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  overflow: scroll;
+`;
+
+export const Nav = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 24px;
+`;
+
+export const Logout = styled.span`
+  font-size: 24px;
+  cursor: pointer;
+
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+export const ProfileName = styled.span`
+  font-size: 24px;
+`;
+
+export const YapControls = styled.div`
+  width: 100%;
+`;
+
+export const YapTabContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: flex-start;
+`;
+
+export const YapTab = styled.span`
+  font-size: 24px;
+  cursor: pointer;
+
+  text-decoration: ${(props) => props.seeMyYaps === props.tabType ? "underline" : "none"};
+
+  :hover {
+    text-decoration: underline;
+  }
+
+  :not(:last-child) {
+    margin-right: 24px;
+  }
+`;
+
+export const Divider = styled.div`
+  width: 100%;
+  border: 2px dashed black;
+  margin-top: 24px;
+  margin-bottom: 24px;
+`;
+
+// NOTE ---------------------------------------------- NEW YAP CONTAINER
+
+export const NewYapContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const YapItBtn = styled.span`
+  cursor: pointer;
+  font-size: 24px;
+
+  :hover {
+    color: green;
+  }
+`;
+
+// NOTE ---------------------------------------------- MY YAPS CONTAINER
+
+export const MyYapsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+`;
+
+export const YapContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: flex-start;
+  width: 100%;
+  padding: 20px 0;
+
+  :not(:last-child) {
+      margin-bottom: 10px;
+      border-bottom: 1px solid black;
+  }
+`;
+
+export const YapDate = styled.span`
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const Yap = styled.p``;
